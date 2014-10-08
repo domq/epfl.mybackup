ac5d29c061da612e9416b50742dcb0773a941050
root <root@lxc.novalocal>
2014-10-08 08:36:12 +0000

    Added clone and start container

--- lxcworker/lxctests.py
+++ lxcworker/lxctests.py
@@ -17,19 +17,44 @@ def get_running_servers_command():
 		print("container IPv4 %r:" % (container.get_ips(interface="eth0", timeout=0.5)))		
 
 def getContainerIP(container_name):
-	container=lxc.Container(container_name)
-	for container_ in lxc.list_containers():
-		if container_==container_name:
-			container = lxc.Container(container_)
-			if (container.state != "STOPPED"):
-				ipaddress=container.get_ips(interface="eth0", timeout=0.5)
-				return ipaddress[0]
-			else:
-				print("the container is stopped")
-				exit()
-			
-	print("no container named "+container_name+" found!")
+	if existsContainer(container_name):
+		container=lxc.Container(container_name)
+		if (container.state != "STOPPED"):
+			ipaddress=container.get_ips(interface="eth0", timeout=0.5)
+			return ipaddress[0]
+		else:
+			print("the container is stopped")
+			exit()
+	else:			
+		print("no container named "+container_name+" found!")
 
+def existsContainer(container_name):
+	container=lxc.Container(container_name)
+	return container.defined
+
+def isStoppedContainer(source_name):
+	container=lxc.Container(source_name)
+	return (container.state == "STOPPED")
+
+		
+	
+
+def cloneAndStartContainer(source_name, new_name):
+	if not existsContainer(source_name):
+		print("container "+source_name+" doesn't exist !")
+		exit()
+	elif existsContainer(new_name):
+		print("container "+new_name+" exists already !")
+		exit()
+	elif not isStoppedContainer(source_name):
+		print("the container "+source_name+" is running, stop it first (lxc-stop -n"+source_name+")")
+		exit()
+	else:
+		container_source=lxc.Container(source_name)
+		container_destination=container_source.clone(new_name)
+		container_destination.start()
+		ipadresses=container_destination.get_ips(timeout=60)
+		print("the container "+new_name+" was started with IP "+ipadresses[0])
 
 
 def matchIptablesRulesOnDPort(dport):
@@ -95,7 +120,11 @@ def deleteIptablesRulesForDestIP(ip):
 		chain.delete_rule(rule)
 	table.commit()
 	table.autocommit = True
-	exit()	
+
+def deleteIptablesRulesForContainer(container_name):
+	container_ip=getContainerIP(container_name)
+	deleteIptablesRulesForDestIP(container_ip)
+	
 
 	
 def addRedirectOfDPort(port,sourceIP,destinationIP):
@@ -124,8 +153,18 @@ def addRedirectOfDPortToContainer(port,sourceIP,containerName):
 	addRedirectOfDPort(port,sourceIP,container_ip)
 
 
+
+
 if len(sys.argv) <2 :
-	print ("please specify a command from [listRulesOnDPort, deleteRulesOnDPort, addRedirect,addRedirectToContainer, deleteRulesForSourceIPi, deleteRulesForDestIP]")
+	print ("please specify a command from :\n\tlistRulesOnDPort [port]\n"+
+						"\tdeleteRulesOnDPort [port]\n"+
+						"\taddRedirect [port sourceIP destinationIP]\n"+
+						"\taddRedirectToContainer [port sourceIP containerName]\n"+
+						"\tdeleteRulesForSourceIP [ip]\n"+
+						"\tdeleteRulesForDestIP [ip]\n"+
+						"\tcloneAndStartContainer [source_name new_name]\n"+
+						"\tgetContainerIP [container_name]\n"+
+						"\tdeleteRedirectToContainer [containerName]")
 	exit()
 else:
 	if sys.argv[1]=="listRulesOnPort":
@@ -134,18 +173,21 @@ else:
 			exit()
 		else:
 			printIptablesRulesOnDPort(sys.argv[2])
+
 	elif sys.argv[1]=="deleteRulesOnPort":
 		if len(sys.argv)!=3:
 			print("specify a port : deleteRulesOnDPort [port]")
 			exit()
 		else:
 			deleteIptablesRulesOnDPort(sys.argv[2])
+
 	elif sys.argv[1]=="deleteRulesForSourceIP":
 		if len(sys.argv)!=3:
 			print("specify an IP : deleteRulesForSourceIP [ip]")
 			exit()
 		else:
 			deleteIptablesRulesForSourceIP(sys.argv[2])
+
 	elif sys.argv[1]=="deleteRulesForDestIP":
 		if len(sys.argv)!=3:
 			print("specify an IP : deleteRulesForDestIP [ip]")
@@ -159,18 +201,36 @@ else:
 			exit()
 		else:
 			addRedirectOfDPort(sys.argv[2], sys.argv[3], sys.argv[4])
+
 	elif sys.argv[1]=="getContainerIP":
 		if len(sys.argv)!=3:
                         print("specify a container name : getContainerIP [containerName]")
                         exit()
 		else:
 			print(getContainerIP(sys.argv[2]))
+
 	elif sys.argv[1]=="addRedirectToContainer":
 		if len(sys.argv)!=5:
 			print("specify a port, source IP and container name : addRedirectToContainer [port sourceIP containerName]")
 			exit()
 		else:
 			addRedirectOfDPortToContainer(sys.argv[2], sys.argv[3], sys.argv[4])
+
+	elif sys.argv[1]=="deleteRedirectToContainer":
+		if len(sys.argv)!=3:
+			print("specify a container name : deleteRedirectToContainer [containerName]")
+			exit()
+		else:
+			deleteIptablesRulesForContainer(sys.argv[2])
+			exit()
+
+	elif sys.argv[1]=="cloneAndStartContainer":
+		if len(sys.argv)!=4:
+			print("specify a source and new container name : cloneAndStartContainer [source_name new_name]")
+			exit()
+		else:
+			cloneAndStartContainer(sys.argv[2], sys.argv[3])
+			exit()
 
 	else:
 		print("unknown command: "+sys.argv[1])
