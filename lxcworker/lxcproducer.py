ac182bf4171030ed142e01b222e26862a12dbfe3
Fedora Cloud User <predrag.viceic@epfl.ch>
2014-10-09 06:59:34 +0000

    changed the 2 queues system to one queue

--- lxcworker/lxcproducer.py
+++ lxcworker/lxcproducer.py
@@ -3,6 +3,9 @@ import pika
 import sys
 import json
 
+timeout=30
+
+
 def callbackresponse(ch, method, properties, body):
 	body = body.decode('utf-8')
 	print(json.dumps(json.loads(body),sort_keys=True,indent=4, separators=(',', ': ')))
@@ -18,26 +21,29 @@ def callbacktimeout():
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='10.0.0.103'))
-connection.add_timeout(30,callbacktimeout)
+connection.add_timeout(timeout,callbacktimeout)
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_lxc_workers',
+# create the topic on the rabbitmq server, if not already done
+channel.exchange_declare(exchange='lxc',
                          type='topic')
 
-channel.exchange_declare(exchange='topic_lxc_master',
-                         type='topic')
-
+#declare the queue on the rabbitmq server, if not already done
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
 
 routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
 message = sys.argv[2]
-channel.basic_publish(exchange='topic_lxc_workers',
+
+# send to the routing key from the commandline the message from the commandline
+channel.basic_publish(exchange='lxc',
                       routing_key=routing_key,
                       body=message)
 print ("[x] Sent %r:%r  -- waiting for reply" % (routing_key, message))
 
-channel.queue_bind(exchange='topic_lxc_master',
+
+#wait the reply for [timeout] seconds
+channel.queue_bind(exchange='lxc',
                        queue=queue_name,
                        routing_key="lxc.master")
 
